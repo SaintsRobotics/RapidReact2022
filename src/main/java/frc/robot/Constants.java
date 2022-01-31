fd0693f0a54190a0534af5a363ef94f440798ffa
ProgrammingSR <programming@saintsrobotics.com>
2022-01-30 17:30:18 -0800

    fix: update robot dimenstions

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,10 +48,10 @@ public final class Constants {
     public static final double kRearRightTurningEncoderOffset = -207;
 
     /** Distance between centers of right and left wheels on robot. */
-    public static final double kTrackWidth = 0.762;
+    public static final double kTrackWidth = 0.57;
 
     /** Distance between front and back wheels on robot. */
-    public static final double kWheelBase = 0.762;
+    public static final double kWheelBase = 0.6;
 
     public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
         new Translation2d(kWheelBase / 2, kTrackWidth / 2), new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
