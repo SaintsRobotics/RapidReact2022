d5b0af49800773f256cfc9f8f64e12fd14fe690f
LennyAbbas <lennyabbas3@gmail.com>
2022-01-30 18:25:05 -0800

    style: whitespace

--- src/main/java/frc/robot/HardwareMap.java
+++ src/main/java/frc/robot/HardwareMap.java
@@ -10,73 +10,73 @@ import frc.robot.subsystems.SwerveModule;
 
 /** File for storing the hardware of the robot. */
 public class HardwareMap {
-        /** File for storing the hardware of the drivetrain. */
-        public class SwerveDrivetrainHardware {
-                public SwerveModule frontLeft = new SwerveModule(
-                                swerveModuleHardware,
-                                swerveModuleHardware.frontLeftDriveMotor,
-                                SwerveConstants.kFrontLeftDriveMotorReversed,
-                                swerveModuleHardware.frontLeftTurningMotor,
-                                swerveModuleHardware.frontLeftEncoder,
-                                SwerveConstants.kFrontLeftTurningEncoderOffset);
-                public SwerveModule rearLeft = new SwerveModule(
-                                swerveModuleHardware,
-                                swerveModuleHardware.rearLeftDriveMotor,
-                                SwerveConstants.kRearLeftDriveMotorReversed,
-                                swerveModuleHardware.rearLeftTurningMotor,
-                                swerveModuleHardware.rearLeftEncoder,
-                                SwerveConstants.kRearLeftTurningEncoderOffset);
-                public SwerveModule frontRight = new SwerveModule(
-                                swerveModuleHardware,
-                                swerveModuleHardware.frontRightDriveMotor,
-                                SwerveConstants.kFrontRightDriveMotorReversed,
-                                swerveModuleHardware.frontRightTurningMotor,
-                                swerveModuleHardware.frontRightEncoder,
-                                SwerveConstants.kFrontRightTurningEncoderOffset);
-                public SwerveModule rearRight = new SwerveModule(
-                                swerveModuleHardware,
-                                swerveModuleHardware.rearRightDriveMotor,
-                                SwerveConstants.kRearRightDriveMotorReversed,
-                                swerveModuleHardware.rearRightTurningMotor,
-                                swerveModuleHardware.rearRightEncoder,
-                                SwerveConstants.kRearRightTurningEncoderOffset);
+  /** File for storing the hardware of the drivetrain. */
+  public class SwerveDrivetrainHardware {
+    public SwerveModule frontLeft = new SwerveModule(
+        swerveModuleHardware,
+        swerveModuleHardware.frontLeftDriveMotor,
+        SwerveConstants.kFrontLeftDriveMotorReversed,
+        swerveModuleHardware.frontLeftTurningMotor,
+        swerveModuleHardware.frontLeftEncoder,
+        SwerveConstants.kFrontLeftTurningEncoderOffset);
+    public SwerveModule rearLeft = new SwerveModule(
+        swerveModuleHardware,
+        swerveModuleHardware.rearLeftDriveMotor,
+        SwerveConstants.kRearLeftDriveMotorReversed,
+        swerveModuleHardware.rearLeftTurningMotor,
+        swerveModuleHardware.rearLeftEncoder,
+        SwerveConstants.kRearLeftTurningEncoderOffset);
+    public SwerveModule frontRight = new SwerveModule(
+        swerveModuleHardware,
+        swerveModuleHardware.frontRightDriveMotor,
+        SwerveConstants.kFrontRightDriveMotorReversed,
+        swerveModuleHardware.frontRightTurningMotor,
+        swerveModuleHardware.frontRightEncoder,
+        SwerveConstants.kFrontRightTurningEncoderOffset);
+    public SwerveModule rearRight = new SwerveModule(
+        swerveModuleHardware,
+        swerveModuleHardware.rearRightDriveMotor,
+        SwerveConstants.kRearRightDriveMotorReversed,
+        swerveModuleHardware.rearRightTurningMotor,
+        swerveModuleHardware.rearRightEncoder,
+        SwerveConstants.kRearRightTurningEncoderOffset);
 
-                public AHRS gyro = new AHRS();
-        }
+    public AHRS gyro = new AHRS();
+  }
 
-        /** File for storing the hardware of the swerve module. */
-        public class SwerveModuleHardware {
-                public CANSparkMax frontLeftTurningMotor = new CANSparkMax(
-                                SwerveConstants.kFrontLeftTurningMotorPort, MotorType.kBrushless);
-                public CANSparkMax frontLeftDriveMotor = new CANSparkMax(
-                                SwerveConstants.kFrontLeftDriveMotorPort, MotorType.kBrushless);
-                public WPI_CANCoder frontLeftEncoder = new WPI_CANCoder(SwerveConstants.kFrontLeftTurningEncoderPort);
+  /** File for storing the hardware of the swerve module. */
+  public class SwerveModuleHardware {
+    public CANSparkMax frontLeftTurningMotor = new CANSparkMax(
+        SwerveConstants.kFrontLeftTurningMotorPort, MotorType.kBrushless);
+    public CANSparkMax frontLeftDriveMotor = new CANSparkMax(
+        SwerveConstants.kFrontLeftDriveMotorPort, MotorType.kBrushless);
+    public WPI_CANCoder frontLeftEncoder = new WPI_CANCoder(SwerveConstants.kFrontLeftTurningEncoderPort);
 
-                public CANSparkMax rearLeftTurningMotor = new CANSparkMax(
-                                SwerveConstants.kRearLeftTurningMotorPort, MotorType.kBrushless);
-                public CANSparkMax rearLeftDriveMotor = new CANSparkMax(
-                                SwerveConstants.kRearLeftDriveMotorPort, MotorType.kBrushless);
-                public WPI_CANCoder rearLeftEncoder = new WPI_CANCoder(SwerveConstants.kRearLeftTurningEncoderPort);
+    public CANSparkMax rearLeftTurningMotor = new CANSparkMax(
+        SwerveConstants.kRearLeftTurningMotorPort, MotorType.kBrushless);
+    public CANSparkMax rearLeftDriveMotor = new CANSparkMax(
+        SwerveConstants.kRearLeftDriveMotorPort, MotorType.kBrushless);
+    public WPI_CANCoder rearLeftEncoder = new WPI_CANCoder(SwerveConstants.kRearLeftTurningEncoderPort);
 
-                public CANSparkMax frontRightTurningMotor = new CANSparkMax(
-                                SwerveConstants.kFrontRightTurningMotorPort, MotorType.kBrushless);
-                public CANSparkMax frontRightDriveMotor = new CANSparkMax(
-                                SwerveConstants.kFrontRightDriveMotorPort, MotorType.kBrushless);
-                public WPI_CANCoder frontRightEncoder = new WPI_CANCoder(SwerveConstants.kFrontRightTurningEncoderPort);
+    public CANSparkMax frontRightTurningMotor = new CANSparkMax(
+        SwerveConstants.kFrontRightTurningMotorPort, MotorType.kBrushless);
+    public CANSparkMax frontRightDriveMotor = new CANSparkMax(
+        SwerveConstants.kFrontRightDriveMotorPort, MotorType.kBrushless);
+    public WPI_CANCoder frontRightEncoder = new WPI_CANCoder(SwerveConstants.kFrontRightTurningEncoderPort);
 
-                public CANSparkMax rearRightTurningMotor = new CANSparkMax(
-                                SwerveConstants.kRearRightTurningMotorPort, MotorType.kBrushless);
-                public CANSparkMax rearRightDriveMotor = new CANSparkMax(
-                                SwerveConstants.kRearRightDriveMotorPort, MotorType.kBrushless);
-                public WPI_CANCoder rearRightEncoder = new WPI_CANCoder(SwerveConstants.kRearRightTurningEncoderPort);
-        }
+    public CANSparkMax rearRightTurningMotor = new CANSparkMax(
+        SwerveConstants.kRearRightTurningMotorPort, MotorType.kBrushless);
+    public CANSparkMax rearRightDriveMotor = new CANSparkMax(
+        SwerveConstants.kRearRightDriveMotorPort, MotorType.kBrushless);
+    public WPI_CANCoder rearRightEncoder = new WPI_CANCoder(SwerveConstants.kRearRightTurningEncoderPort);
+  }
 
-        public SwerveModuleHardware swerveModuleHardware;
-        public SwerveDrivetrainHardware swerveDrivetrainHardware;
+  public SwerveModuleHardware swerveModuleHardware;
+  public SwerveDrivetrainHardware swerveDrivetrainHardware;
 
-        /** Creates a new {@link HardwareMap}. */
-        public HardwareMap() {
-                swerveModuleHardware = new SwerveModuleHardware();
-                swerveDrivetrainHardware = new SwerveDrivetrainHardware();
-        }
+  /** Creates a new {@link HardwareMap}. */
+  public HardwareMap() {
+    swerveModuleHardware = new SwerveModuleHardware();
+    swerveDrivetrainHardware = new SwerveDrivetrainHardware();
+  }
 }
