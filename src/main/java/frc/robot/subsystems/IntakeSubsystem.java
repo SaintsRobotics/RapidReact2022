500eadc48a7ba2b22a627e8fd597572b411c2af1
dbzfan2012 <ethank2784@gmail.com>
2022-02-04 17:00:07 -0800

    Feat: Added feeder features to intake subsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -14,18 +14,30 @@ public class IntakeSubsystem extends SubsystemBase {
 
   private CANSparkMax m_intakeController;
   private CANSparkMax m_armController;
+  private CANSparkMax m_feederController;
   private double m_desiredInputSpeed;
+  private double m_desiredFeederSpeed;
 
   /** Creates a new IntakeSubsystem. */
   public IntakeSubsystem(IntakeHardware intake) {
     m_intakeController = intake.intakeController;
     m_armController = intake.armController;
+    m_feederController = intake.feederController;
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     m_intakeController.set(m_desiredInputSpeed);
+    m_feederController.set(m_desiredFeederSpeed);
+  }
+
+  public void turnFeederOn() {
+    m_desiredFeederSpeed = 1;
+  }
+
+  public void turnFeederOff() {
+    m_desiredFeederSpeed = 0;
   }
 
   public void moveArm(double velocity) {
