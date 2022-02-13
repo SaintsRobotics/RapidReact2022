e7b312a795f233d0589e5a7ea311e60f8dcfb105
LennyAbbas <lennyabbas3@gmail.com>
2022-02-12 19:23:47 -0800

    style: changed indentation to tab size 2

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -15,65 +15,65 @@ import frc.robot.Constants.SwerveConstants;
 
 /** Class that controls the swerve wheel and reads the swerve encoder. */
 public class SwerveModule {
-  private final CANSparkMax m_driveMotor;
-  private final CANSparkMax m_turningMotor;
+	private final CANSparkMax m_driveMotor;
+	private final CANSparkMax m_turningMotor;
 
-  private final AbsoluteEncoder m_turningEncoder;
+	private final AbsoluteEncoder m_turningEncoder;
 
-  private final PIDController m_turningPIDController = new PIDController(0.3, 0, 0);
+	private final PIDController m_turningPIDController = new PIDController(0.3, 0, 0);
 
-  /**
-   * Creates a new {@link SwerveModule}.
-   * 
-   * @param driveMotor     motor that drives the wheel
-   * @param turningMotor   motor that changes the angle of the wheel
-   * @param turningEncoder absolute encoder for the swerve module
-   */
-  public SwerveModule(CANSparkMax driveMotor, CANSparkMax turningMotor, AbsoluteEncoder turningEncoder) {
-    m_driveMotor = driveMotor;
-    m_turningMotor = turningMotor;
+	/**
+	 * Creates a new {@link SwerveModule}.
+	 * 
+	 * @param driveMotor     motor that drives the wheel
+	 * @param turningMotor   motor that changes the angle of the wheel
+	 * @param turningEncoder absolute encoder for the swerve module
+	 */
+	public SwerveModule(CANSparkMax driveMotor, CANSparkMax turningMotor, AbsoluteEncoder turningEncoder) {
+		m_driveMotor = driveMotor;
+		m_turningMotor = turningMotor;
 
-    m_turningEncoder = turningEncoder;
+		m_turningEncoder = turningEncoder;
 
-    m_driveMotor.getEncoder().setVelocityConversionFactor(ModuleConstants.kWheelCircumferenceMeters
-        / 60 / ModuleConstants.kDrivingGearRatio);
-        
-    m_turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
-    m_driveMotor.setIdleMode(IdleMode.kBrake);
-    m_turningMotor.setIdleMode(IdleMode.kBrake);
-  }
+		m_driveMotor.getEncoder().setVelocityConversionFactor(ModuleConstants.kWheelCircumferenceMeters
+				/ 60 / ModuleConstants.kDrivingGearRatio);
 
-  /**
-   * Returns the current state of the module.
-   *
-   * @return The current state of the module.
-   */
-  public SwerveModuleState getState() {
-    return new SwerveModuleState(m_driveMotor.getEncoder().getVelocity(), m_turningEncoder.get());
-  }
+		m_turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
+		m_driveMotor.setIdleMode(IdleMode.kBrake);
+		m_turningMotor.setIdleMode(IdleMode.kBrake);
+	}
 
-  /**
-   * Stops the module from driving and turning. Use this so the wheels don't reset
-   * to straight.
-   */
-  public void setDesiredState() {
-    m_turningMotor.set(0);
-    m_driveMotor.set(0);
-  }
+	/**
+	 * Returns the current state of the module.
+	 *
+	 * @return The current state of the module.
+	 */
+	public SwerveModuleState getState() {
+		return new SwerveModuleState(m_driveMotor.getEncoder().getVelocity(), m_turningEncoder.get());
+	}
 
-  /**
-   * Sets the desired state for the module.
-   *
-   * @param desiredState Desired state with speed and angle.
-   */
-  public void setDesiredState(SwerveModuleState desiredState) {
-    SwerveModuleState state = SwerveModuleState.optimize(desiredState, m_turningEncoder.get());
+	/**
+	 * Stops the module from driving and turning. Use this so the wheels don't reset
+	 * to straight.
+	 */
+	public void setDesiredState() {
+		m_turningMotor.set(0);
+		m_driveMotor.set(0);
+	}
 
-    final double driveOutput = state.speedMetersPerSecond / SwerveConstants.kMaxSpeedMetersPerSecond;
-    final double turnOutput = m_turningPIDController.calculate(m_turningEncoder.get().getRadians(),
-        state.angle.getRadians());
+	/**
+	 * Sets the desired state for the module.
+	 *
+	 * @param desiredState Desired state with speed and angle.
+	 */
+	public void setDesiredState(SwerveModuleState desiredState) {
+		SwerveModuleState state = SwerveModuleState.optimize(desiredState, m_turningEncoder.get());
 
-    m_driveMotor.set(driveOutput);
-    m_turningMotor.set(turnOutput);
-  }
+		final double driveOutput = state.speedMetersPerSecond / SwerveConstants.kMaxSpeedMetersPerSecond;
+		final double turnOutput = m_turningPIDController.calculate(m_turningEncoder.get().getRadians(),
+				state.angle.getRadians());
+
+		m_driveMotor.set(driveOutput);
+		m_turningMotor.set(turnOutput);
+	}
 }
