9ac303b142234384857945956ea4e5173fb1ec26
ProgrammingSR <programming@saintsrobotics.com>
2022-02-20 16:21:20 -0800

    docs: added doc for Port

--- src/main/java/frc/robot/MUX.java
+++ src/main/java/frc/robot/MUX.java
@@ -9,10 +9,11 @@ import edu.wpi.first.wpilibj.I2C;
 public class MUX {
 	private final I2C m_I2C = new I2C(I2C.Port.kMXP, 0x70);
 
+	/** Represents a port for the mux as a single byte array. */
 	public enum Port {
-		kOne(new byte[] { (byte) 1 }), // single byte array to configure mux for port 1 0b00000001
-		kTwo(new byte[] { (byte) 2 }), // single byte array to configure mux for port 2 0b00000010
-		kThree(new byte[] { (byte) 4 }); // single byte array to configure mux for port 3 0b00000100
+		kOne(new byte[] { (byte) 1 }),
+		kTwo(new byte[] { (byte) 2 }),
+		kThree(new byte[] { (byte) 4 });
 
 		public final byte[] value;
 
