9939274c7fe8a32b61c8bc6f0c331b64a4917f2b
Skippyyy <programming@saintsrobotics.com>
2022-03-12 16:25:03 -0800

    chore: got rid of 1 in javadoc

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -140,7 +140,7 @@ public class RobotContainer {
 
 	/**
 	 * Use this to pass the autonomous command to the main {@link Robot} class.
-	 * 
+	 * 1
 	 * @return the command to run in autonomous
 	 */
 	public Command getAutonomousCommand() {
