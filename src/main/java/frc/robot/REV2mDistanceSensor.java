5b7e2cc9d9e235bff4283894aa6728311c7e092c
ProgrammingSR <programming@saintsrobotics.com>
2022-03-14 15:57:49 -0700

    docs: added docs to Rev2mDistanceSensor

--- src/main/java/frc/robot/REV2mDistanceSensor.java
+++ src/main/java/frc/robot/REV2mDistanceSensor.java
@@ -1,31 +1,40 @@
 package frc.robot;
 
-import com.revrobotics.ColorSensorV3;
 import com.revrobotics.Rev2mDistanceSensor;
 import com.revrobotics.Rev2mDistanceSensor.Unit;
 
-import edu.wpi.first.wpilibj.I2C;
-
+/**
+ * Wrapper for the {@link Rev2mDistanceSensor} object from RevLib. Most of the
+ * javadocs for these methods are the same.
+ */
 public class REV2mDistanceSensor {
     private final MUX m_mux;
     private final MUX.Port m_port;
     private final Rev2mDistanceSensor m_sensor;
 
+    /**
+     * Creates a new {@link Rev2mDistanceSensor}.
+     * 
+     * @param mux  The {@link MUX} that the distance sensor is connected to.
+     * @param port The port on the {@link MUX} that the distance sensor is connected
+     *             to.
+     */
     public REV2mDistanceSensor(MUX mux, MUX.Port port) {
         m_mux = mux;
         m_port = port;
         m_mux.switchToPort(m_port);
-		m_sensor = new Rev2mDistanceSensor(Rev2mDistanceSensor.Port.kMXP);
+        m_sensor = new Rev2mDistanceSensor(Rev2mDistanceSensor.Port.kMXP);
         m_sensor.setAutomaticMode(true);
     }
 
     /**
      * Returns distance from sensor.
-     * @return distance in meters
+     * 
+     * @return Distance in meters.
      */
     public double getDistance() {
-		m_mux.switchToPort(m_port);
+        m_mux.switchToPort(m_port);
         return m_sensor.getRange(Unit.kMillimeters) / 1000;
     }
-  
+
 }
